8cb34b06b4ae46e812dd3361a3aaf3f9fc15100c
pushkining <pushkining@ua.fm>
2019-11-02 12:48:20 +0200

    Fopr

--- main.js
+++ main.js
@@ -9,19 +9,38 @@ function render1(posts){
     var html = '';
     for(var post of posts){
         html += '<div class="post">';
-        html +=   '<p>' + post.title + '</p>';
+        html +=   '<b>' + post.data_update + '</b>';
+        html +=   '<h2>' + post.title + '</h2>';
+        html +=   '<p>' + post.description + '</p>';
         html += '</div>'
     }
     return html;
 }
 function render2(str){
     var container = document.querySelector('#list-posts');
-    console.dir(container);
     container.innerHTML = str
 }
 
-
+function showPosts() {
 fetch('http://blog.api.axenov-it.com/posts')
     .then(format)
     .then(render1)
     .then(render2);
+}
+
+var btn = document.querySelector('#render-posts');
+
+var isOpen = false;
+
+function toogle() {
+    var container = document.querySelector('#list-posts');
+    if (isOpen) {
+        container.innerHTML = "";
+        isOpen = false;
+    }else{
+        showPosts();
+        isOpen = true;
+    }
+}
+
+btn.onclick = showPosts;
